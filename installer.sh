46224c754a7f52cf2439592df09aa5275b27cd3e
root <root@localhost.localdomain>
2016-08-12 10:46:26 +0900

    ddd

---
+++ installer.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+# yum update
+for ((i=0;i<COLUMNS;i++));do
+	echo -n "-"
+done
+echo -e "\nOperating Yum Update"
+for ((i=0;i<COLUMNS;i++));do
+	echo -n "-"
+done
+sudo yum update
+
+sleep 1
+
+# Repository
+for ((i=0;i<COLUMNS;i++));do
+	echo -n "-"
+done
+echo -e "\nRepository Download"
+for ((i=0;i<COLUMNS;i++));do
+	echo -n "-"
+done
+
+sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+
+sleep 1
+
+# install puppet
+for ((i=0;i<COLUMNS;i++));do
+	echo -n "-"
+done
+echo -e "\nInstall Puppet"
+for ((i=0;i<COLUMNS;i++));do
+	echo -n "-"
+done
+
+sudo yum install -y puppetserver
+
+sudo puppet module install puppetlabs-stdlib
+
