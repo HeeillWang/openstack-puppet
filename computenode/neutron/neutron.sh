cb2765805f8f3f6e54b0e344435bdbf89fc0e4a6
root <root@localhost.localdomain>
2016-08-13 14:43:25 +0900

    fix error for package

--- computenode/neutron/neutron.sh
+++ computenode/neutron/neutron.sh
@@ -1,6 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+pwd
 
-puppet apply neutron-pakage.pp
+puppet apply neutron-package.pp
 puppet apply neutron-conf.pp
 puppet apply neutron-ovs.pp
