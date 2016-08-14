7633405eb33a887fba4fcd536b2429d8620fb4b2
showx123 <showx123@naver.com>
2016-08-14 17:34:26 +0900

    keystone changed

--- contoller_node/keystone/keystone.sh
+++ contoller_node/keystone/keystone.sh
@@ -4,8 +4,8 @@ cd $DIR
 
 #Create database and an administration token on mysql
 mysql -u root mysql -e "CREATE DATABASE keystone"
-mysql -u root mysql -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'skcc1234'"
-mysql -u root mysql -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'skcc1234'"
+mysql -u root mysql -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS'"
+mysql -u root mysql -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS'"
 
 openssl rand -hex 10 > /root/rand_hex.txt
 
@@ -14,6 +14,8 @@ openssl rand -hex 10 > /root/rand_hex.txt
 sudo puppet apply keystone-package.pp
 #Config keystone.conf
 sudo puppet apply keystone_conf.pp
+#Populate the Identity service database
+/bin/sh -c "keystone-manage db_sync" keystone
 #Config httpd.conf
 sudo puppet apply httpd_conf.pp
 #Config wsgi-keystone.conf
