b04d6f4b85fe4128a2b0cb6d1d19964b995b3fcb
root <root@localhost.localdomain>
2016-08-15 16:31:09 +0900

    160815

--- computenode/nova/test.pp
+++ computenode/nova/test.pp
@@ -1,6 +1,14 @@
-augeas { "sshd_config":
-  context => "/files/etc/ssh/sshd_config",
-  changes => [
-    "set PermitRootLogin no",
-  ],
+#augeas { "test":
+#  context => '/files/root/openstack-puppet/computenode/nova/test.txt',
+#  changes => [
+#    "set puppet yes",
+#  ],
+#}
+
+file_line { 'someline':
+  path     => '/root/openstack-puppet/computenode/nova/test.txt',
+  match    => '#puppet',
+  line     => 'puppet=yes',
+  after    => '[default]',
+  multiple => 'false',
 }
