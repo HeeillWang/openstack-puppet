4ffe024cb7d96e2a95fcd6fd1cf8a2e45cc85b7f
showx123 <showx123@naver.com>
2016-08-16 15:32:34 +0900

    compute_nova completed

--- computenode/nova/exec.pp
+++
@@ -1,5 +0,0 @@
-exec{'abc':
-  command => "sed -i '3s|!!!|def|g' test.txt",
-  cwd => "/root/openstack-puppet/computenode/nova",
-  path => "/bin",
-}
--- computenode/nova/nova_conf.pp
+++ computenode/nova/nova_conf.pp
@@ -24,13 +24,14 @@ class nova_conf($path = "/etc/nova/nova.conf"){
    }
  
    exec{'auth_strategy':
-      command => "sed -i '506s|#||g' nova.conf",
+      command => "sed -i '/\[DEFAULT\]/,/\[api_database\]/s/#auth_strategy=keystone/auth_strategy = keystone/g' nova.conf",
       cwd => "/etc/nova",
       path => "/bin",
    }
 
+   #add lines only one time
    if file("/etc/nova/nova.conf") =~ /\#auth_uri/{
-   file_line{'auth_uri':
+   file_line{'keystone_authtoken':
       path	=> $path,
       line	=>
 "auth_uri = http://controller:5000
@@ -45,6 +46,77 @@ password = skcc1234",
    }
    }
 
+   file_line{'my_ip':
+      path	=> $path,
+      line	=> "my_ip = 10.0.2.15",
+      match	=> "#my_ip",
+   }
+
+   file_line{'network_api_class':
+      path	=> $path,
+      line	=> "network_api_class = nova.network.neutronv2.api.API",
+      match	=> "#network_api_class",
+   }
+
+   file_line{'security_group_api':
+      path	=> $path,
+      line	=> "security_group_api = neutron",
+      match	=> "#security_group_api",
+   }
+
+   file_line{'linuxnet_interface_driver':
+      path	=> $path,
+      line	=> "linuxnet_interface_driver = nova.network.linux_net.NeutronLinuxBridgeInterfaceDriver",
+      match	=> "#linuxnet_interface_driver",
+   }
+
+   file_line{'firewall_driver':
+      path	=> $path,
+      line	=> "firewall_driver = nova.virt.firewall.NoopFirewallDriver",
+      match	=> "#firewall_driver",
+   }
+
+   exec{'vnc_enabled':
+      command => "sed -i '/\[vnc\]/,/\[workarounds\]/s/#enabled=true/enabled = true/g' nova.conf",
+      cwd => "/etc/nova",
+      path => "/bin",
+   }
+
+   file_line{'vncserver_listen':
+      path	=> $path,
+      line	=> "vncserver_listen = 0.0.0.0",
+      match	=> "#vncserver_listen",
+   }
+
+   file_line{'server_proxyclient_address':
+      path	=> $path,
+      line	=> "vncserver_proxyclient_address = \$my_ip",
+      match	=> "#vncserver_proxyclient_address",
+   }
+
+   file_line{'novncproxy_base_url':
+      path	=> $path,
+      line	=> "novncproxy_base_url = http://10.0.2.15:6080/vnc_auto.html",
+      match	=> "#novncproxy_base_url",
+   }
+
+   exec{'glance_host':
+      command => "sed -i '/\[glance\]/,/\[guestfs\]/s/#host=\$my_ip/host = controller/g' nova.conf",
+      cwd => "/etc/nova",
+      path => "/bin",
+   }
+
+   file_line{'lock_path':
+      path	=> $path,
+      line	=> "lock_path = /var/lib/nova/tmp",
+      match	=> "#lock_path",
+   }
+
+   file_line{'verbose':
+      path	=> $path,
+      line	=> "verbose = True",
+      match	=> "#verbose",
+   }
 }
 
 include nova_conf
