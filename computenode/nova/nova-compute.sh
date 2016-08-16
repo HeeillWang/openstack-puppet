489680341a76f05a45d94f38df801658885955dd
showx123 <showx123@naver.com>
2016-08-16 17:21:49 +0900

    add finalize step

--- computenode/nova/nova-compute.sh
+++ computenode/nova/nova-compute.sh
@@ -1,2 +1,5 @@
 #Install the packages
 sudo yum install -y openstack-nova-compute sysfsutils
+sudo puppet apply nova_conf.pp
+sudo systemctl enable libvirtd.service openstack-nova-compute.service
+sudo systemctl start libvirtd.service openstack-nova-compute.service
