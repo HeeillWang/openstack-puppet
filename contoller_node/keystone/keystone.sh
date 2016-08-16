014fe97a309fac96ef8244620e25366d961ca990
showx123 <showx123@naver.com>
2016-08-16 19:14:09 +0900

    keystone started

---
+++ contoller_node/keystone/keystone.sh
@@ -0,0 +1,9 @@
+#Create database and an administration token on mysql
+
+mysql -u root mysql -e "CREATE DATABASE keystone2"
+mysql -u root mysql -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS'"
+mysql -u root mysql -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS'"
+
+openssl rand -hex 10 > rand_hex.txt
+
+
