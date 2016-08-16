6bfedd0821cb54d3841a7ae48a5cf37c3c8dd727
showx123 <showx123@naver.com>
2016-08-16 16:44:38 +0900

    trouble shooting for mariaDB

--- contoller_node/MySQL/mariadb.pp
+++ contoller_node/MySQL/mariadb.pp
@@ -5,14 +5,14 @@ package {'MySQL-python':}
 file{ 'mariadb_openstack.cnf':
 	path	=> '/etc/my.cnf.d/mariadb_openstack.cnf',
 	content	=>
-'[mysqld]
-bind-address = 0.0.0.0
+"[mysqld]
+bind-address = 10.0.2.15
 default-storage-engine = innodb
-inodb_file_per_table
+innodb_file_per_table
 collation-server = utf8_general_ci
-init-connect-set-server = utf8
-max_connections = 500
-'
+init-connect = 'SET NAMES utf8'
+character-set-server = utf8
+"
 }
 
 service {'mariadb':
