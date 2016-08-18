665e65b866b1c8bef5abe6a464f9ba26211ccc2c
root <chot1198@gmail.com>
2016-08-18 18:06:41 +0900

    glance

--- contoller_node/glance/glance.sh
+++ contoller_node/glance/glance.sh
@@ -6,13 +6,37 @@ sudo mysql -u root mysql -e " GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' I
 # Create uesr, endpoint, service entry
 source admin-openrc.sh
 
+for((i=0;i<COLUMNS;i++))do
+	echo '-'
+done
+echo 'Create Openstack User: glacne...'
 openstack user create --domain default --password skcc1234 glance
+
+for((i=0;i<COLUMNS;i++))do
+        echo '-'
+done
+echo 'Add user role: admin...'
 openstack role add --project service --user glance admin
+for((i=0;i<COLUMNS;i++))do
+        echo '-'
+done
+echo 'Create Openstack Service: glance...'
 openstack service create --name glance --description "OpenStack Image service" image
+for((i=0;i<COLUMNS;i++))do
+        echo '-'
+done
+echo 'Create Image Endpoint...'
 openstack endpoint create --region RegionOne image public http://controller:9292
 openstack endpoint create --region RegionOne image internal http://controller:9292
 openstack endpoint create --region RegionOne image admin http://controller:9292
 
+
+puppet apply glance_package.pp
 puppet apply glance_api.pp
 puppet apply glance_regi.pp
+
+
+
+
+
 su -s /bin/sh -c "glance-manage db_sync" glance
