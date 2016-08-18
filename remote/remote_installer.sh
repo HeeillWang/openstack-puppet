d40749c9eecfac08659664cb37a807038e6b60a0
root <root@localhost.localdomain>
2016-08-18 14:03:21 +0900

    remote fixed

--- remote/remote_installer.sh
+++ remote/remote_installer.sh
@@ -4,26 +4,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-echo "			Start Remote install in compute-node"
-
-if [ -e /root/.ssh/id_rsa ];then
-	echo "		RSA-key already exist! skip RSA-key generation"
-else
-	echo "		Generate RSA-key..."
-	ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-fi
-
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_rsa
-chmod 644 ~/.ssh/id_rsa.pub
-chmod 644 ~/.ssh/known_hosts || true
-
-echo '			copy rsa-key to compute node'
-ssh-copy-id root@compute
-#scp ~/.ssh/id_rsa.pub root@compute:~/id_rsa.pub
-#ssh compute 'mkdir ~/.ssh;cat ~/id_rsa.pub >> ~/.ssh/authorized_keys'
-#ssh compute 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;cat ~/id_rsa.pub >> ~/.ssh/authorized_keys'
-
 echo '			install git...'
 ssh compute 'yum install -y git'
 echo '			git cloning...'
