86d4e3bddbebe6eec3d01843b05e18be70a83adc
showx123 <showx123@naver.com>
2016-08-19 15:13:35 +0900

    nova completed

--- contoller_node/nova/nova.sh
+++ contoller_node/nova/nova.sh
@@ -1,17 +1,17 @@
 #Create database
-#mysql -u root -p"KEYSTONE_DBPASS" mysql -e "CREATE DATABASE nova"
-#mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS'"
-#mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS'"
+mysql -u root -p"KEYSTONE_DBPASS" mysql -e "CREATE DATABASE nova"
+mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS'"
+mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS'"
 
 #Create Nova service, user, role and endpoints
 source /root/admin-openrc.sh
 
-#openstack user create --domain default --password skcc1234 nova
-#openstack role add --project service --user nova admin
-#openstack service create --name nova --description "OpenStack Compute" compute
-#openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
+openstack user create --domain default --password skcc1234 nova
+openstack role add --project service --user nova admin
+openstack service create --name nova --description "OpenStack Compute" compute
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
 
 
 #Populate the Compute database
