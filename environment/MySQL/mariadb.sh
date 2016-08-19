2988dce000e60255fdb4e8d245c83cd72418b600
showx123 <showx123@naver.com>
2016-08-19 11:32:54 +0900

    move 'set mysql root password'

--- environment/MySQL/mariadb.sh
+++ environment/MySQL/mariadb.sh
@@ -2,3 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 puppet apply mariadb.pp
+
+#Set mysql root password
+mysql -u root mysql -e "update user set password=password('KEYSTONE_DBPASS') where user='root';flush privileges;"
