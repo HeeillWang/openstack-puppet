3046bc8033cf7bf26c323b3e1b58caa28f199e50
showx123 <showx123@naver.com>
2016-08-19 17:35:31 +0900

    add package

---
+++ contoller_node/nova/nova-package.pp
@@ -0,0 +1,7 @@
+package{'openstack-nova-api':}
+package{'openstack-nova-cert':}
+package{'openstack-nova-conductor':}
+package{'openstack-nova-console':}
+package{'openstack-nova-novncproxy':}
+package{'openstack-nova-scheduler':}
+package{'python-novaclient':}
