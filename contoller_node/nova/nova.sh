e1b9830d418f19f41e76a3a7c7745a3adda8e54f
showx123 <showx123@naver.com>
2016-08-19 18:08:37 +0900

    add missing manifest file

--- contoller_node/nova/nova.sh
+++ contoller_node/nova/nova.sh
@@ -3,22 +3,25 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 #Create database
-#mysql -u root -p"KEYSTONE_DBPASS" mysql -e "CREATE DATABASE nova"
-#mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS'"
-#mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS'"
+mysql -u root -p"KEYSTONE_DBPASS" mysql -e "CREATE DATABASE nova"
+mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS'"
+mysql -u root -p"KEYSTONE_DBPASS" mysql -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS'"
 
 #Create Nova service, user, role and endpoints
 #source /root/admin-openrc.sh
 
-#openstack user create --domain default --password skcc1234 nova
-#openstack role add --project service --user nova admin
-#openstack service create --name nova --description "OpenStack Compute" compute
-#openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
-#openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
+openstack user create --domain default --password skcc1234 nova
+openstack role add --project service --user nova admin
+openstack service create --name nova --description "OpenStack Compute" compute
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
 
 #install the packages
 sudo puppet apply nova-package.pp
+
+#configuring nova.conf
+sudo puppet apply nova_conf.pp
 
 #Populate the Compute database
 /bin/sh -c "nova-manage db sync" nova
