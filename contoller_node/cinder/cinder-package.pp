84ab764732538e4adba1b73782bc513a8a85661d
showx123 <showx123@naver.com>
2016-08-24 11:37:15 +0900

    cinder compelted

--- contoller_node/cinder/cinder-package.pp
+++ contoller_node/cinder/cinder-package.pp
@@ -1,16 +1,6 @@
 class cinder-package(){
    package{'openstack-cinder':}
    package{'python-cinderclient':}
-   package{'lvm2':}
-   package{'targetcli':}
-   package{'python-oslo-policy':}   
 }
 
 include cinder-package
-
-service{'lvm2-lvmetad':
-   ensure   => running,
-   enable   => true, 
-}
-
-Class['cinder-package'] -> Service['lvm2-lvmetad']
