6df87c9a91d611db67bd15b9cd28360d7a89848e
root <chot1198@gmail.com>
2016-08-25 13:46:48 +0900

    remote installer

---
+++ remote/remote_installer.sh
@@ -0,0 +1,22 @@
+#Move to current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
+chmod 644 ~/.ssh/known_hosts
+
+scp ~/.ssh/id_rsa.pub root@compute:~/id_rsa.pub
+ssh compute 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;cat ~/id_rsa.pub >> ~/.ssh/authorized_keys'
+
+#scp compute.sh root@compute:~/compute.sh
+#ssh compute "source compute.sh"
+
+ssh compute 'yum install -y git'
+ssh compute 'cd /root;git clone https://github.com/showx123/openstack-puppet'
+ssh compute 'source openstack-puppet/puppet-install.sh'
+ssh compute 'source openstack-puppet/environment/environment.sh'
+ssh comptue 'source openstack-puppet/compute/compute.sh'
