217a79d73c43f6a18a30c9e5b12473ae2607227a
root <chot1198@gmail.com>
2016-08-25 16:27:49 +0900

    remote

--- remote/remote_installer.sh
+++ remote/remote_installer.sh
@@ -1,6 +1,9 @@
+
+
 #Move to current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
@@ -11,12 +14,15 @@ chmod 644 ~/.ssh/known_hosts
 
 scp ~/.ssh/id_rsa.pub root@compute:~/id_rsa.pub
 ssh compute 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;cat ~/id_rsa.pub >> ~/.ssh/authorized_keys'
-
-#scp compute.sh root@compute:~/compute.sh
-#ssh compute "source compute.sh"
-
+echo 'install git...'
 ssh compute 'yum install -y git'
+echo 'git cloning...'
 ssh compute 'cd /root;git clone https://github.com/showx123/openstack-puppet'
-ssh compute 'sudo source openstack-puppet/puppet-install.sh'
-ssh compute 'sudo source openstack-puppet/environment/environment.sh'
-ssh comptue 'sudo source openstack-puppet/compute/compute.sh'
+echo 'answer file copy...'
+scp $DIR/../answer.txt root@compute:/root/openstack-puppet/answer.txt
+echo 'run puppet-install.sh...'
+ssh -t compute 'source openstack-puppet/puppet-install.sh'
+echo 'run environment.sh...'
+ssh -t compute 'source openstack-puppet/environment/environment.sh'
+echo 'run compute.sh...'
+ssh -t comptue 'source openstack-puppet/compute/compute.sh'
