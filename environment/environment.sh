df8158a0c2dc60d7babe45593b5897498973805e
showx123 <showx123@naver.com>
2016-08-29 19:13:06 +0900

    rabbitmq fixed

--- environment/environment.sh
+++ environment/environment.sh
@@ -7,15 +7,16 @@ cd $DIR
 #export environment variable FACTERLIB
 export FACTERLIB="$DIR/custom_facts"
 
-if [ $hostname != controller ];then
+
+if [ "$hostname" != "controller" ];then
 	hostnamectl set-hostname compute
 fi
-#installing step
+installing step
 puppet apply ./host/host.pp
 ./ntp/ntp.sh
 ./openstack-package/openstack-package.sh
 ./MySQL/mariadb.sh
-#./NoSQL/nosql.sh
-./rabbitmq/rabbitmq.sh
+./NoSQL/nosql.sh
+ssh controller /root/openstack-puppet/environment/rabbitmq/rabbitmq.sh
 
 echo "Environment install completed!"
